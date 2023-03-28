03ecd27bb84d72d2aac9cff4ac65bd3a471fb31c
HARSH VARDHAN PRASAD <114021101+harsh7274v@users.noreply.github.com>
2023-03-28 09:48:38 +0530

    Create program to print an integer.c

---
+++ program to print an integer.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+  int num;
+printf("enter an integer");
+  // reads and stores input
+scanf("%d",&num);
+  //displays output
+ printf("you entered:%d",num);
+  return 0;
+}
