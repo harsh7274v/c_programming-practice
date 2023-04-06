025e181e07808d73575ad2374acbcd7f38b11ca9
HARSH VARDHAN PRASAD <114021101+harsh7274v@users.noreply.github.com>
2023-04-07 02:36:16 +0530

    Create program to check leap year.c

---
+++ program to check leap year.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+int num;
+  printf("enter the year");
+  scanf("%d",&num);
+  if (num % 400 == 0){
+    printf("%d is a leap year",num);
+  }
+  else if (num % 100 == 0){
+    printf("%d is not leap year",year);
+  else if (num %4 == 0){
+printf("%d, is a leap year",year);
+    else{
+      printf("%d is not a leap year",year);
+    }
+    return 0;
+  }
+     
